ec442f0467716cbadda939f53c85151751d9af2c
JiaLeng99 <ngjl-wm17"student.tarc.edu.my>
2019-10-24 11:23:45 +0800

    .

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice(){
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+
+        diceImage = findViewById(R.id.dice_image1)
+        diceImage.setImageResource(rep())
+        diceImage = findViewById(R.id.dice_image2)
+        diceImage.setImageResource(rep())
+        diceImage = findViewById(R.id.dice_image3)
+        diceImage.setImageResource(rep())
+
+    }
+
+    private fun rep():Int{
+
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        return drawableResource
+    }
+
+}
